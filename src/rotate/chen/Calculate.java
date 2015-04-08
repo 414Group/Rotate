faaaffc37b3a8f68439cd88456400ad30cd00e69
cheng10 <cheng10@ualberta.ca>
2015-04-08 00:27:48 -0600

    asf

--- src/rotate/chen/Calculate.java
+++ src/rotate/chen/Calculate.java
@@ -41,27 +41,17 @@ public class Calculate
     return after;
   }
   
-  public Double[] rotateyxz(Double[] vectorzyx, Double[] theta)
+  public Double[] rotateyxz(Double[] vectorxyz, Double[] theta)
   {
     Double[] afterxyz = { Double.valueOf(0.0D), Double.valueOf(0.0D), Double.valueOf(0.0D) };
-    Double[] vectorxyz=new Double[3];
-    Double [] afterzyx=new Double[3];
     Double ztheta = theta[0];
-    Double ytheta = theta[2];
-    Double xtheta = theta[1];
-    
-    vectorxyz[0]=vectorzyx[2];
-    vectorxyz[1]=vectorzyx[1];
-    vectorxyz[2]=vectorzyx[0];
+    Double ytheta = theta[1];
+    Double xtheta = theta[2];
    
     afterxyz = yrotate(vectorxyz, ytheta);
     afterxyz = xrotate(afterxyz, xtheta);
     afterxyz = zrotate(afterxyz, ztheta);
-   
-    afterzyx[0]=afterxyz[2];
-    afterzyx[1]=afterxyz[1];
-    afterzyx[2]=afterxyz[0];
     
-    return afterzyx;
+    return afterxyz;
   }
 }
